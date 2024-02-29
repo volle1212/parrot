1494dd62a958692dc99ca08b6214ac947747e549
volle1212 <94704890+volle1212@users.noreply.github.com>
2024-02-29 10:31:48 +0100

    simplify for cmd usage

--- parrot.py
+++ parrot.py
@@ -8,13 +8,23 @@ clear = lambda: os.system('cls')
 frames = [f for f in listdir("C:/Users/vilmer.folcke/Documents/python/projects/celebration/frames") if isfile(join("C:/Users/vilmer.folcke/Documents/python/projects/celebration/frames", f))]
 start_time = time.perf_counter()
 
-colors = ["\033[1;31;40m", "\033[1;31;40m", "\033[1;33;40m", "\033[1;32;40m", "\033[1;32;40m", "\033[1;31;40m", "\033[1;36;40m", "\033[1;34;40m", "\033[1;34;40m",  "\033[1;35;40m" "\033[1;37;40m"]
+all_colors = {
+    "red": "\033[1;31;40m",
+    "green": "\033[1;32;40m",
+    "yellow": "\033[1;33;40m",
+    "blue": "\033[1;34;40m",
+    "magenta": "\033[1;35;40m",
+    "cyan": "\033[1;36;40m",
+    "white": "\033[1;37;40m",
+              }
+
+colors = [all_colors.get('red'), all_colors.get('red'), all_colors.get('yellow'), all_colors.get('green'), all_colors.get('green'), all_colors.get('red'), all_colors.get('cyan'), all_colors.get('blue'), all_colors.get('blue'),  all_colors.get('magenta'), all_colors.get('white')]
 
 def party_if_accepted(i):
     """Takes number of parrot iterations as integer"""
     accept = False
     while not accept:
-        if input(f"{rainbowtext.text('Do you want to celebrate?')}\033[0;37;40m (\033[0;32;40my\033[0;37;40m/\033[0;31;40mn\033[0;37;40m)\n(Some console data might get lost)").lower() == "y":
+        if input('Do you want to celebrate? (y/n)\n(console logs will get lost)').lower() == "y":
             accept = True
     for _ in range(i):
         for frame in frames:
