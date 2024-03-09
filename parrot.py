1736dc22e140e5890f67a94709d770e73a164e29
volle1212 <folckevilmer@gmail.com>
2024-03-09 21:03:22 +0100

    added new window for parrot

--- parrot.py
+++ parrot.py
@@ -2,10 +2,12 @@ import os
 from os import listdir
 from os.path import isfile, join
 import time
-import rainbowtext
+import subprocess
+
+path_to_files = "frames/"
 
 clear = lambda: os.system('cls')
-frames = [f for f in listdir("C:/Users/vilmer.folcke/Documents/python/projects/celebration/frames") if isfile(join("C:/Users/vilmer.folcke/Documents/python/projects/celebration/frames", f))]
+frames = [f for f in listdir(path_to_files) if isfile(join(path_to_files, f))]
 start_time = time.perf_counter()
 
 all_colors = {
@@ -16,21 +18,16 @@ all_colors = {
     "magenta": "\033[1;35;40m",
     "cyan": "\033[1;36;40m",
     "white": "\033[1;37;40m",
-              }
+}
 
 colors = [all_colors.get('red'), all_colors.get('red'), all_colors.get('yellow'), all_colors.get('green'), all_colors.get('green'), all_colors.get('red'), all_colors.get('cyan'), all_colors.get('blue'), all_colors.get('blue'),  all_colors.get('magenta'), all_colors.get('white')]
 
-def party_if_accepted(i, path_to_files=""):
+def party(i, path_to_files=path_to_files):
     """Takes number of parrot iterations as integer 
     and path to the frames which has to be named 0.txt, 1.txt etc with a max of 9.txt"""
     if path_to_files == "":
         raise Exception("Missing path to files (path_to_files, takes string)")
 
-    accept = False
-    while not accept:
-        print("")
-        if input('Do you want to celebrate? (y/n)\n(console logs will get lost)').lower() == "y":
-            accept = True
     for _ in range(i):
         for frame in frames:
             color = colors[int(frame.replace(".txt", ""))]
@@ -49,4 +46,12 @@ def party_if_accepted(i, path_to_files=""):
     print("\033[0;37;40m")
     clear()
 
-party_if_accepted(5, "C:/Users/vilmer.folcke/Documents/python/projects/celebration/frames/")
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2 or sys.argv[1] != "party":
+        command = f"start cmd /k python parrot.py party"
+        subprocess.Popen(command, shell=True)
+        sys.exit(1)
+
+    party(5)
